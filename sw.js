4ba0034e20720c41d35d1318410360f0847b97f0
Andrea Cambindo <94268966+AndreaCambindo@users.noreply.github.com>
2026-09-11 20:24:48 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@
    app para que los usuarios reciban la versión nueva.
    ===================================================== */
 
-const CACHE_VERSION = 'monly-cache-v4';
+const CACHE_VERSION = 'monly-cache-v5';
 
 const PRECACHE_URLS = [
   './',
